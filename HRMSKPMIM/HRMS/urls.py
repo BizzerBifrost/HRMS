af67ef8d322912ed2524bc1b02dbed33c848ee30
BizzerBifrost <bizzerbifrost@gmail.com>
2025-05-18 22:18:30 +0800

    index.html

--- HRMSKPMIM/HRMS/urls.py
+++ HRMSKPMIM/HRMS/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     # home
-
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
     # staff
 
     # hr
--- HRMSKPMIM/HRMS/views.py
+++ HRMSKPMIM/HRMS/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        'page': 'index'  # Add context to help with template debugging
+    }
+    return render(request, 'index.html', context)
+
+def login(request):
+    return render(request, 'login.html')
